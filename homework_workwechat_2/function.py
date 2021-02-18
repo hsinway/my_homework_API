f0c4c6af5da69e296186fe083bf599b808e7a385
hsinway <xinwei01@live.com>
2021-02-18 17:23:42 +0800

    update

--- homework_workwechat_2/function.py
+++ homework_workwechat_2/function.py
@@ -3,5 +3,9 @@ from faker import Faker
 
 
 def fake_userids_create():
+    """
+    利用faker生成随机测试id
+    :return:
+    """
     userids = [Faker().bothify(text="?????##") for i in range(20)]
     yaml.dump(userids, open('./file/userid.yaml', 'w', encoding='UTF-8'))
